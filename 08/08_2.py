c14cdbe9a5a4fcd0f8bde927e62174feff8f9bf7
Hiromasa Kato <hka@trackman.com>
2021-12-08 17:26:47 +0100

    cleaned

--- 08/08_2.py
+++ 08/08_2.py
@@ -1,79 +1,98 @@
 import numpy as np
 import itertools
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.WARN)
 
 
 # 0123456
 # abcdefg
 
+
 def do_one(ten, output):
 
     src2dst = [set()] * 7
+    
+    done = set()
 
     # cf
     ten_2 = [v for v in filter(lambda l: len(l) == 2, ten)][0]
-    print(ten_2)
     src2dst[2] = set(ten_2)
     src2dst[5] = set(ten_2)
-    done = set(ten_2)
+    done = done.union(set(ten_2))
+    logging.debug(f"after 2-element codes {ten_2}, done = {done}")
+    logging.debug(f"src2dst = {src2dst}")
 
-    # acf
+    # a of acf
     ten_3 = [v for v in filter(lambda l: len(l) == 3, ten)][0]
-    print(ten_3)
-    i3 = set(ten_3) - set(ten_2)
-    c3 = i3.pop()
-    i3 = ord(c3) - ord("a")
-    src2dst[0] = set([c3])
-    done.add(c3)
-
-    print(src2dst)
+    src2dst[0] = set(ten_3) - done
+    done = done.union(set(ten_3))
+    logging.debug(f"after 3-element code {ten_3}, done = {done}")
+    logging.debug(f"src2dst = {src2dst}")
 
     # bd of bcdf
     ten_4 = [v for v in filter(lambda l: len(l) == 4, ten)][0]
-    print(ten_4)
     src2dst[1] = set(ten_4) - done
     src2dst[3] = set(ten_4) - done
     done = done.union(set(ten_4))
-    print("src2dst = ", src2dst)
+    logging.debug(f"after 4-element code {ten_4}, done = {done}")
+    logging.debug(f"src2dst = {src2dst}")
 
     # eg of abcdefg
     ten_7 = [v for v in filter(lambda l: len(l) == 7, ten)][0]
-    print(ten_7, set(ten_7) - done)
     src2dst[4] = set(ten_7) - done
     src2dst[6] = set(ten_7) - done
-    print("src2dst = ", src2dst)
+    done = done.union(set(ten_7))
+    logging.debug(f"after 7-element code {ten_7}, done = {done}")
+    logging.debug("we now completed the search space")
+    logging.debug(f"src2dst = {src2dst}")
+
+    # at this point, the search space is now fixed, the solution is a subset of src2dst.
 
     # acdeg, acdfg, abdfg
-    # adg is common
+    # adg is common, bcef is not shared.
     ten_5 = [set(v) for v in filter(lambda l: len(l) == 5, ten)]
-    print(ten_5)
+    logging.debug(f"analyzing 5-element outputs: {ten_5}")
     common = ten_5[0].intersection(ten_5[1]).intersection(ten_5[2])
-    print(common)
     src2dst[0] = src2dst[0].intersection(common)
     src2dst[3] = src2dst[3].intersection(common)
     src2dst[6] = src2dst[6].intersection(common)
-    print("src2dst = ", src2dst)
+    logging.debug(f"after 5-element codes, narrowing the space by 3 common elements (adg): {common}")
+    logging.debug(f"src2dst = {src2dst}")
+
+    extra = ten_5[0].union(ten_5[1]).union(ten_5[2]) - common
+    src2dst[1] = src2dst[1].intersection(extra)
+    src2dst[2] = src2dst[2].intersection(extra)
+    src2dst[4] = src2dst[4].intersection(extra)
+    src2dst[5] = src2dst[5].intersection(extra)
+    logging.debug(f"after 5-element codes, narrowing the space by 3 unshared elements (bcef): {extra}")
+    logging.debug(f"src2dst = {src2dst}")
 
     # abcefg, abdefg, abcdfg
-    # abfg is common
+    # abfg is common, cde is not shared
     ten_6 = [set(v) for v in filter(lambda l: len(l) == 6, ten)]
-    print(ten_6)
-    common = ten_6[0].intersection(ten_6[1]).intersection(ten_6[2])
-    print(common)
+    logging.debug(f"analyzing 6-element outputs: {ten_6}")
+    common = set.intersection(*ten_6)
     src2dst[0] = src2dst[0].intersection(common)
     src2dst[1] = src2dst[1].intersection(common)
     src2dst[5] = src2dst[5].intersection(common)
     src2dst[6] = src2dst[6].intersection(common)
-    print("src2dst = ", src2dst)
+    logging.debug(f"after 6-element codes, narrowing the space by 4 common elements (abfg): {common}")
+    logging.debug(f"src2dst = {src2dst}")
 
-    fixed = set([list(f)[0] for f in src2dst if len(f) == 1])
-    print("fixed = ", fixed)
-    for i, d in enumerate(src2dst):
-        if len(d) > 1:
-            src2dst[i] = d.difference(fixed)
-    print("src2dst = ", src2dst)
+    extra = set.union(*ten_6) - common
+    src2dst[2] = src2dst[2].intersection(extra)
+    src2dst[3] = src2dst[3].intersection(extra)
+    src2dst[4] = src2dst[4].intersection(extra)
+    logging.debug(f"after 6-element codes, narrowing the space by 3 unshared elements (cde): {extra}")
+    logging.debug(f"src2dst = {src2dst}")
+    logging.debug("we now completed the mapping")
+    for d in src2dst:
+        assert len(d) == 1
 
+    # we build a map now.
     src2dst = [list(d)[0] for d in src2dst]
-    print("src2dst = ", src2dst)
     src = "abcdefg"
     src2dst_map = {}
     for i, d in enumerate(src2dst):
